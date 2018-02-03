031a2542a63151c72f2b799c83359ac93d6c3f72
Erik Navarrete <leonavas1995@gmail.com>
2018-02-03 11:38:43 +0100

    Creando structuras, solo la estructura sin utilizar

--- 3 Arrays/3.2Estructuras.c
+++ 3 Arrays/3.2Estructuras.c
@@ -10,13 +10,22 @@
         punto v2;
         punto v3;
     }triangulo;
-    
 
-int main(){
+    typedef struct stTitular{
+        char nombre[10];
+        char apellido[10];
+        char direccion[20];
+        char dni[10];
+    } clientes;
 
+    typedef struct stCuentaCorriente{
+        clientes c1;
+        clientes c2;
+        int n_cuenta[20];
+        double saldo;
+    }cuenta;
     
-    
-  
+int main(){  
 
     return 0;
 }
