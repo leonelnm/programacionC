a62a5e61974304894e5cf17c929eddf5e51c2487
Erik Navarrete <leonavas1995@gmail.com>
2018-02-16 00:51:57 +0100

    Ordenar vector mediante Burbuja

--- Ejercicios FeedBack/Ejercicio9.c
+++ Ejercicios FeedBack/Ejercicio9.c
@@ -2,30 +2,58 @@
 
 #define N 10
 
-void barrasAsteriscos (int *v){
+int numeroMax(int *v){
+    int max = 0;
 
-    for(int i = 0; i < N; i++)
-    {
-        int p = v[i];
-        for(int j = 0; j < p ; j++)
-        {
-            printf("*\n");
+    for(int i = 0; i < N; i++){
+        if(v[i]>max)
+            max = v[i];
+    }
+
+    return max;
+}
+
+void barrasAsteriscos (int *v, int max){
+    for(int i = 0; i < max; i++){
+        for(int j=0; j<N; j++){
+            if(v[j] >= i)
+                printf(" *");
+            else
+                printf("  ");
         }
+        printf("\n");
     }
     return;
 }
 
-
+void ordenarVector(int *v){
+    int temp;
+    for(int i = 0; i < N-1; i++)
+    {
+        for(int j = i+1; j < N; j++)
+        {
+            if (v[j]>v[i]){
+                temp = v[j];
+                v[j] = v[i];
+                v[i] = temp;
+            }
+        }
+    }
+    return;
+}
 int main(){
 
-    int v[N] = {2,4,6,8,5,12,14,16,18,20};
+    int v[N] = {21,4,6,20,5,12,14,16,18,20};
+    int max = numeroMax(v);
+    barrasAsteriscos(v, max);
     
-    barrasAsteriscos(v);
+    ordenarVector(v);
+    printf("Vector ordenado\n");
+    barrasAsteriscos(v, max);
     
-
-    /*//Salida
+    //Salida
     printf("\nPulsa INTRO para finalizar...");
     fflush(stdin);
-    char tecla = getchar();*/
+    char tecla = getchar();
     return 0;
 }
